6ad3b7608c31c595d1468bc73b7c83cafab0e286
Liora13 <liorabr@yahoo.com>
2022-07-07 20:15:35 +0200

    End Crud

--- src/Form/ProduitFormType.php
+++ src/Form/ProduitFormType.php
@@ -4,15 +4,15 @@ namespace App\Form;
 
 use App\Entity\Produit;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Validator\Constraints\Image;
-use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Image;
 
 class ProduitFormType extends AbstractType
 {
@@ -66,18 +66,18 @@ class ProduitFormType extends AbstractType
             ])
             ->add('photo', FileType::class, [
                 'label' => "Photo du produit",
-//                'data_class' => null,
+                'data_class' => null,
                 'constraints' => [
                     new Image([
                         'mimeTypes' => ['image/jpeg', 'image/png'],
                         'mimeTypesMessage' => 'Les formats autorisés sont .jpg ou .png',
                         'maxSize' => '3M',
-                        'maxSizeMessage' => 'Le poids maximal du fichier est : {{ limit }} {{ suffix }} ({{ name }}: {{ size }} {{ suffix }})'
+                        'maxSizeMessage' => 'Le poids maximal du fichier est : {{ limit }} {{ suffix }} ({{ name }}: {{ size }} {{ suffix }})',
                     ])
-                    ]
+                ]
             ])
             ->add('submit', SubmitType::class, [
-                'label' => 'Ajouter',
+                'label' => $options['photo'] ? 'Modifier' : 'Ajouter',
                 'validate' => false,
                 'attr' => [
                     'class' => 'd-block mx-auto col-3 btn btn-success'
