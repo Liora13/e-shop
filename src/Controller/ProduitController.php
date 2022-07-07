5742ec03de61888dcc58b25b60884ca108a58472
Liora13 <liorabr@yahoo.com>
2022-07-07 12:31:04 +0200

    webpack +update produit

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -7,6 +7,7 @@ use App\Form\ProduitFormType;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -41,14 +42,39 @@ class ProduitController extends AbstractController
             $produit->setCreatedAt(new DateTime());
             $produit->setUpdatedAt(new DateTime());
 
+            # Récupération du fichier dans le formulaire. Ce sera un objet de type UploadedFile.
             /** @var UploadedFile $photo */
             $photo = $form->get('photo')->getData();
 
-            # 1 Déconstruire le nom du fichier
+
+            if ($photo) {
+
+                 # 1 Déconstruire le nom du fichier
+            $extension = '.' . $photo->guessExtension();
             # 2 Variabiliser tous les éléments du nouveau nom de fichier après sécurisation
+                #le slug assainit le title du produit (il retire les accents, les espaces, les majuscules)
+            $safeFilename = $slugger->slug($produit->getTitle());
             # 3 Reconstruit du nom du fichier
+            $newFilename = $safeFilename . '_' . uniqid() . $extension;
             # 4 Déplacement du fichier temporaire dans un dossier permanent dans notre projet.
-        }
+
+            #try/catch s'utilise lorsqu'une méthode lance (throws) une erreur (Exception)
+            try {
+                $photo->move($this->getParameter('uploads_dir'), $newFilename);
+                $produit->setPhoto($newFilename);
+            } catch (FileException $exception) {
+                $this->addFlash('warning', "Une erreur est survenue pendant l'upload de votre fichier :( Veuillez recommencer.");
+                # On redirige le user et notre script s'arretera là si il rentre dans le catch()
+                return $this->redirectToRoute('show_produits');
+            } # end catch ()
+            } # end if($photo)
+           
+            $entityManager->persist($produit);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Le produit est en ligne avec succès !');
+            return $this->redirectToRoute('show_produits');
+        }# end if($form)
 
         return $this->render("admin/form/form_produit.html.twig", [
             'form' => $form->createView()
